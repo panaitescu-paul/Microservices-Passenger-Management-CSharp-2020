e39535f67c63bc8b4fc429bf1635edd6818d2830
MariusMM2 <dmunteanu91@gmail.com>
2020-04-21 10:17:04 +0200

    initial commit - framework

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace passenger_management.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
