9b6f1f9be8a7e39cbe10eb256af5f72580c96dc3
Constantin <razvan1598@gmail.com>
2020-04-21 10:29:50 +0200

    test

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace passenger_management
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            // razvan commit
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
